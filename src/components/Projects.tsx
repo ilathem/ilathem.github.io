9b8e20ce082f5d39130ca44bce97c4becc2fdd2a
ilathem <isaiahlathem@gmail.com>
2022-07-12 10:57:16 -0500

    project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Project from './Project';
+import "../styles/projects.scss"
 
 function Projects() {
 
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="projects-container">
+      <h1 className="projects-header">Projects</h1>
+
+      <Project 
+        previewImg='portfolio-preview.png'
+        title="Portfolio"
+        desc="Personal portfolio site"
+        tech="TypeScript, SCSS, React"
+        ghLink="https://github.com/ilathem/ilathem.github.io"
+      />
     </div>
   )
 }
